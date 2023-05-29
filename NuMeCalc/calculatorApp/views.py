b4264a23126ee69221714b620e43efb1b11fe815
juanfepi27 <85038450+juanfepi27@users.noreply.github.com>
2023-05-28 21:10:37 -0500

    se comienza a dar formato a los elementos del capitulo 2

--- NuMeCalc/calculatorApp/views.py
+++ NuMeCalc/calculatorApp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 import matlab.engine #Download this library by doing python -m pip install matlabengine
 import pandas as pd
 
@@ -18,6 +19,9 @@ def biseccion(request):
     xs = 3.0
     tol = 0.005
     typeTol = 0
+    # tipos:
+    # - 0 -> dc
+    # - 1 -> cs
     niter = 100.0 
     fun = '(x^3+5)-2' #read the function given
     eng.code_biseccion(xi,xs,tol,typeTol,niter,fun) #call the function in matlab, be careful because the matlab file has to be in the same address of this code
@@ -31,6 +35,9 @@ def secante(request):
     x1 = 3.5
     tol = 0.005
     typeTol = 0
+    # tipos:
+    # - 0 -> dc
+    # - 1 -> cs
     niter = 100.0 
     fun = '(x^3+5)-2' #read the function given
     eng.code_secante(x0,x1,tol,typeTol,niter,fun) #call the function in matlab, be careful because the matlab file has to be in the same address of this code
@@ -43,6 +50,9 @@ def newtonRaph(request):
     x0 = 3.0   #Be careful to put all these data in float type!
     tol = 0.005
     typeTol = 0
+    # tipos:
+    # - 0 -> dc
+    # - 1 -> cs
     niter = 100.0 
     fun = '(x^3+5)-2' #read the function given
     eng.code_newtonRaph(x0,tol,typeTol,niter,fun) #call the function in matlab, be careful because the matlab file has to be in the same address of this code
@@ -52,6 +62,34 @@ def newtonRaph(request):
 
 # -----------------------------------------Capítulo 2--------------------------------------------------------
 
+def goMatrix(request,method):
+    if(method==0):
+        return render(request,'calculatorApp/cap2-selNxN.html',context={'method':'jacobi'})
+    elif(method==1):
+        return render(request,'calculatorApp/cap2-selNxN.html',context={'method':'gaussSeid'})
+    elif(method==2):
+        return render(request,'calculatorApp/cap2-selNxN.html',context={'method':'sor'})
+    else:
+        messages.error(request,"Estas ingresando incorrectamente a un metodo matricial")
+        return redirect('selectionPage')
 
+def matJacobiSeidSor(request):
+    #Arguments we need to do the function
+    tol = 0.005
+    typeTol = 0
+    # tipos:
+    # - 0 -> dc
+    # - 1 -> cs
+    niter = 1000.0 
+    met=2
+    # métodos:
+    # - 0 -> jacobi
+    # - 1 -> gauss-seidel
+    # - 2 -> SOR
+    w=0.5
+    eng.code_MatJacobiSeidSor(tol,typeTol,niter,met,w) #call the function in matlab, be careful because the matlab file has to be in the same address of this code
+    df = pd.read_csv('data_iterativos.csv')
+    print(df)
+    return render(request, 'calculatorApp/biseccion.html',context={})
 
 # -----------------------------------------Capítulo 3--------------------------------------------------------
